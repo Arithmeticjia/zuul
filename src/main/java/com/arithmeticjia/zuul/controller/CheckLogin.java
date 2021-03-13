5c98a13c5bb485c382836be9d0558cf522566b29
ArithmeticJia <1097197237@qq.com>
2021-03-13 20:50:09 +0800

    add reconsitution api

--- src/main/java/com/arithmeticjia/zuul/controller/CheckLogin.java
+++ src/main/java/com/arithmeticjia/zuul/controller/CheckLogin.java
@@ -1,6 +1,7 @@
 package com.arithmeticjia.zuul.controller;
 
 import com.arithmeticjia.zuul.annotation.UserLoginToken;
+import com.arithmeticjia.zuul.api.CommonResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,9 @@ public class CheckLogin {
 
     @UserLoginToken
     @GetMapping("/check-login")
-    public String checkLogin() {
-        return "HelloWord";
+    public CommonResult checkLogin() {
+        String data = "HelloWorld";
+        return CommonResult.success(data);
     }
 }
 
